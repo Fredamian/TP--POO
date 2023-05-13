6980bdee231cd2579a11bb0f6b68eb407022a81a
Fredamian <101630567+Fredamian@users.noreply.github.com>
2023-05-13 15:31:09 -0400

    Delete foxrabbitsv2 directory

--- foxrabbitsv2/FoxAndRabbit.java
+++
@@ -1,8 +0,0 @@
-package foxrabbitsv2;
-
-public class FoxAndRabbit {
-    public static  void main(String[] args) {
-        Simulador simulador = new Simulador();
-        simulador.runLongSimulation();
-    }
-}
